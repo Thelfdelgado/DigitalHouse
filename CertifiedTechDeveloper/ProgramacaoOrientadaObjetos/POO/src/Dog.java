adb30c6a4d5af0139a06b10a01bbe6275ceb485c
Luiz Delgado <luiz.delgadov@gmail.com>
2022-05-25 20:28:23 -0300

    pOO 2

---
+++ CertifiedTechDeveloper/ProgramacaoOrientadaObjetos/POO/src/Dog.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+import java.time.Year;
+
+public class Dog {
+    private String name;
+    private Double weight;
+    private String breed;
+    private Year birthDate;
+    private Boolean hasTag;
+    private Boolean isVaccinated = false;
+    private Boolean isNeutered = false;
+    private Boolean isUpForAdoption;
+
+    public Dog(
+            String name,
+            Double weight,
+            String breed,
+            Year birthDate,
+            Boolean hasTag,
+            Boolean isUpForAdoption,
+    )
+        public getAge() {
+        private Year currentYear = LocalDate.now().getYear();
+        }
+}
