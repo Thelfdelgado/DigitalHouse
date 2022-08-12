e64f200ffa3e60d465884b822cb7d6cb8a90c5a3
Luiz Delgado <luiz.delgadov@gmail.com>
2022-08-11 22:46:43 -0300

    Aula12

--- CertifiedTechDeveloper/BackEnd/BackEnd1/Aula12_Integradora/src/Mesa/Main.java
+++ CertifiedTechDeveloper/BackEnd/BackEnd1/Aula12_Integradora/src/Mesa/Main.java
@@ -1,5 +1,6 @@
 package Mesa;
 
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.h2.Driver;
 
@@ -18,7 +19,7 @@ public class Main {
 
     private static final String sqlInsert1 = "INSERT INTO Funcionarios(id, nome, sobrenome, cargo, idade) VALUES (1, 'Luiz', 'Delgado', 'DevJunior', 24)";
     private static final String sqlInsert2 = "INSERT INTO Funcionarios(id, nome, sobrenome, cargo, idade) VALUES (2, 'Luiz1', 'Delgado1', 'DevPleno', 25)";
-    private static final String sqlInsert3 = "INSERT INTO Funcionarios(id, nome, sobrenome, cargo, idade) VALUES (3, 'Luiz2', 'Delgado2', 'DevSenior', 26)";
+    private static final String sqlInsert3 = "INSERT INTO Funcionarios(id, nome, sobrenome, cargo, idade) VALUES (1, 'Luiz2', 'Delgado2', 'DevSenior', 26)";
 
     private static final String sqlUpdate = "UPDATE Funcionarios SET cargo = 'DevSenior' WHERE cargo = 'DevPleno'";
     private static final String sqlUpdateId = "UPDATE Funcionarios SET id = 3 WHERE nome = 'Luiz2'";
@@ -26,6 +27,7 @@ public class Main {
     private static final String sqlDelete = "DELETE FROM Funcionarios WHERE nome='Luiz'";
 
     public static void main(String[] args) throws Exception {
+        BasicConfigurator.configure();
         Connection c = null;
 
         try {
@@ -36,44 +38,33 @@ public class Main {
             s.execute(sqlInsert1);
             s.execute(sqlInsert2);
             s.execute(sqlInsert3);
-            logger.info("Usuarios com mesmo id");
-
-            ShowFuncionarios(c);
-            logger.info("Mostra os funcionarios");
-
-            s.execute(sqlUpdate);
             s.execute(sqlUpdateId);
-            logger.debug("Update debug");
-
+            s.execute(sqlUpdate);
             s.execute(sqlDelete);
             s.execute(sqlDeleteId);
-            ShowFuncionarios(c);
-            logger.info("Mostra os funionarios atualizados");
         }
-        catch (Exception e){
+        catch (org.h2.jdbc.JdbcSQLIntegrityConstraintViolationException e){
+            logger.info("Id duplicada");
+            logger.error("Id ja existente");
             e.printStackTrace();
-            logger.error("Ids repetidos.");
         }finally {
-            if (c == null){
-                return;
+            String sqlQuery = "SELECT * FROM Funcionarios";
+            Statement s = c.createStatement();
+            ResultSet rs = s.executeQuery(sqlQuery);
+
+            while (rs.next()){
+                System.out.println(rs.getInt(1) + " - "
+                + rs.getString(2) + " - "
+                + rs.getString(3) + " - "
+                + rs.getString(4) + " - "
+                + rs.getInt(5));
             }
-            c.close();
         }
     }
 
-    private static void ShowFuncionarios(Connection c) throws SQLException {
-        String sqlQuery = "SELECT * FROM Funcionarios";
-        Statement s = c.createStatement();
-        ResultSet rs = s.executeQuery(sqlQuery);
+//    private static void ShowFuncionarios(Connection c) throws SQLException {
 
-        while (rs.next()){
-            System.out.println(rs.getInt(1) + " - "
-            + rs.getString(2) + " - "
-            + rs.getString(3) + " - "
-            + rs.getString(4) + " - "
-            + rs.getInt(5));
-        }
-    }
+
 
     public static Connection getConnection() throws Exception {
         Class.forName("org.h2.Driver").newInstance();
