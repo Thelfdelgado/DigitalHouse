6c29ac90fba305c0f414ebce6c3976d451962abf
Luiz Delgado <luiz.delgadov@gmail.com>
2022-10-19 20:59:04 -0300

    Aula 3

---
+++ CertifiedTechDeveloper/FrontEnd/front_end_III/Aula_3/Mesa/src/App.jsx
@@ -0,0 +1,11 @@
+function App(){
+  return (
+    <div className="App">
+      <h1 className="title">Random</h1>
+      <img src="https://picsum.photos/200/300" alt="" />
+      <h3>Image Generator</h3>
+    </div>
+  );
+}
+
+export default App
