f508efb1a296634018ed397b5d2dd5385b528cdc
Luiz Delgado <luiz.delgadov@gmail.com>
2022-11-13 17:01:56 -0300

    prova

--- CertifiedTechDeveloper/FrontEnd/front_end_III/Aula_10/prova/src/App.jsx
+++ CertifiedTechDeveloper/FrontEnd/front_end_III/Aula_10/prova/src/App.jsx
@@ -1,18 +1,73 @@
-import Card from './Card';
+import { useState } from 'react';
+import Card from './Card'
 
 function App() {
+  const [formData, setFormData] = useState({ veiculo: '', placa: '' });
+  const [carros, setCarros] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // const newMulta = Boolean(Math.round(Math.random() < 0.5));
+
+    // function valiMulta() {
+    //   if (newMulta === true) {
+    //     return "Possui Multa!";
+    //   } else {
+    //     return "Não Possui Multa!";
+    //   }
+    // };
+
+    // var resMulta = valiMulta();
+    // console.log(valiMulta());
+
+    const valiPlaca = new RegExp('^(?=.*[a-z])(?=.*?[0-9]).{6,}$');
+    var newVeiculo = formData.veiculo.trim();
+
+    if (formData.veiculo === '' || formData.placa === '') {
+      alert('Preencha todos os campos');
+    } else if (formData.veiculo.length < 3) {
+      alert('Username com menos de 3 caracteres');
+    } else if (!valiPlaca.test(formData.placa)) {
+      alert('Insira a placa corretamente')
+    } else {
+      setCarros([...carros, {
+        // multa: resMulta,
+        veiculo: newVeiculo,
+        placa: formData.placa
+      }]);
+    }
+  };
+
   return (
-    <div className='App'>
-      <h1>Cadastre-se</h1>
-      <form>
-        <label>
-          Nome:
-          <input type="text" />
+    <div>
+      <h1>Consulta Placa</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Veiculo
+          <input value={formData.veiculo}
+            onChange={(event) => setFormData({ ...formData, veiculo: event.target.value })}
+            placeholder={'Ex.: Kadett'} />
         </label>
+
+        <label>Placa
+          <input value={formData.placa}
+            onChange={(event) => setFormData({ ...formData, placa: event.target.value })}
+            placeholder={'Ex.: abc1234'} />
+        </label>
+
+        <button type='submit' onClick={handleSubmit}>Consultar</button>
       </form>
-      <Card />
+
+      {/* <div>{JSON.stringify(carros)}</div> */}
+
+      {
+        carros.map((carro) => {
+          return (
+            <Card carro={carro} />
+          )
+        })
+      }
     </div>
-  );
-};
+  )
+}
 
 export default App;
--- CertifiedTechDeveloper/FrontEnd/front_end_III/Aula_10/prova/src/Card.jsx
+++ CertifiedTechDeveloper/FrontEnd/front_end_III/Aula_10/prova/src/Card.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+const Card = (props) => {
 
-const Card = () => {
+
+    function valiMulta() {
+        const newMulta = Boolean(Math.round(Math.random() < 0.5));
+        if (newMulta === true) {
+            return "Possui Multa!";
+        } else {
+            return "Não Possui Multa!";
+        }
+    };
+
+    console.log(valiMulta());
+
     return (
-        <div>
-            
+        <div className="Card">
+            <h2>Multa: {valiMulta()}</h2>
+            <h3>Veiculo: {props.carro.veiculo}</h3>
+            <h3>Placa: {props.carro.placa}</h3>
         </div>
-    );
-};
+    )
+}
 
 export default Card;
