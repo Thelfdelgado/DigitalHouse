d9ceaad49f261b60215365b6b8de383571e1ebb4
Thelfdelgado <luiz.delgadov@gmail.com>
2023-05-19 17:41:12 -0300

    update

---
+++ BackEndSpecialization/Modulo_2/Aula_4/Product-Service/src/main/java/com/example/dh/ProductService/ProductPerviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.dh.ProductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPerviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPerviceApplication.class, args);
+	}
+
+}
---
+++ BackEndSpecialization/Modulo_2/Aula_4/Product-Service/src/test/java/com/example/dh/ProductService/ProductPerviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dh.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPerviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
